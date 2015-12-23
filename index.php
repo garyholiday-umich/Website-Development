c606614283936bac6cc32904d4a204ce7394e0e1
Gary Johnson <gjjohnson13@gmail.com>
2015-12-23 15:57:15 -0500

    Add videos

--- index.php
+++ index.php
@@ -150,181 +150,21 @@
             </div>
         </div>
     </section>
-    <section>
+    <section id="work">
         <div class="container text-center wow fadeIn">
             <h2>Portfolio</h2>
             <hr class="colored">
-            <p>Here are some other projects that I've worked on.</p>
-            <div class="row content-row">
-                <div class="col-lg-12">
-                    <div class="portfolio-filter">
-                        <ul id="filters" class="clearfix">
-                            <li>
-                                <span class="filter active" data-filter="identity graphic logo web">All</span>
-                            </li>
-                            <li>
-                                <span class="filter" data-filter="identity">Identity</span>
-                            </li>
-                            <li>
-                                <span class="filter" data-filter="graphic">Graphic</span>
-                            </li>
-                            <li>
-                                <span class="filter" data-filter="web">Web</span>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            <p>A few of my recordings</p>
             <div class="row">
-                <div class="col-lg-12">
-                    <div id="portfoliolist">
-                        <div class="portfolio identity" data-cat="identity" href="#portfolioModal1" data-toggle="modal">
-                            <div class="portfolio-wrapper">
-                                <img src="assets/img/portfolio/grid/identity/1.jpg" alt="">
-                                <div class="caption">
-                                    <div class="caption-text">
-                                        <a class="text-title">Generic Inc.</a>
-                                        <span class="text-category">Brand Identity</span>
-                                    </div>
-                                    <div class="caption-bg"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio web" data-cat="web" href="#portfolioModal2" data-toggle="modal">
-                            <div class="portfolio-wrapper">
-                                <img src="assets/img/portfolio/grid/web/1.jpg" alt="">
-                                <div class="caption">
-                                    <div class="caption-text">
-                                        <a class="text-title">Brands</a>
-                                        <span class="text-category">Web Development</span>
-                                    </div>
-                                    <div class="caption-bg"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio graphic" data-cat="graphic" href="#portfolioModal3" data-toggle="modal">
-                            <div class="portfolio-wrapper">
-                                <img src="assets/img/portfolio/grid/graphic/1.jpg" alt="">
-                                <div class="caption">
-                                    <div class="caption-text">
-                                        <a class="text-title">Excellence</a>
-                                        <span class="text-category">Poster Design</span>
-                                    </div>
-                                    <div class="caption-bg"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio web" data-cat="web" href="#portfolioModal4" data-toggle="modal">
-                            <div class="portfolio-wrapper">
-                                <img src="assets/img/portfolio/grid/web/2.jpg" alt="">
-                                <div class="caption">
-                                    <div class="caption-text">
-                                        <a class="text-title">YouTV</a>
-                                        <span class="text-category">Web Development</span>
-                                    </div>
-                                    <div class="caption-bg"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio graphic" data-cat="graphic" href="#portfolioModal1" data-toggle="modal">
-                            <div class="portfolio-wrapper">
-                                <img src="assets/img/portfolio/grid/graphic/2.jpg" alt="">
-                                <div class="caption">
-                                    <div class="caption-text">
-                                        <a class="text-title">Office Supplies</a>
-                                        <span class="text-category">Graphic Design</span>
-                                    </div>
-                                    <div class="caption-bg"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio web" data-cat="web" href="#portfolioModal2" data-toggle="modal">
-                            <div class="portfolio-wrapper">
-                                <img src="assets/img/portfolio/grid/web/3.jpg" alt="">
-                                <div class="caption">
-                                    <div class="caption-text">
-                                        <a class="text-title">Fooder</a>
-                                        <span class="text-category">Mobile App</span>
-                                    </div>
-                                    <div class="caption-bg"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio graphic" data-cat="graphic" href="#portfolioModal3" data-toggle="modal">
-                            <div class="portfolio-wrapper">
-                                <img src="assets/img/portfolio/grid/graphic/3.jpg" alt="">
-                                <div class="caption">
-                                    <div class="caption-text">
-                                        <a class="text-title">Mountain Tea</a>
-                                        <span class="text-category">Graphic Design</span>
-                                    </div>
-                                    <div class="caption-bg"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio graphic" data-cat="graphic" href="#portfolioModal4" data-toggle="modal">
-                            <div class="portfolio-wrapper">
-                                <img src="assets/img/portfolio/grid/graphic/4.jpg" alt="">
-                                <div class="caption">
-                                    <div class="caption-text">
-                                        <a class="text-title">Checkmark</a>
-                                        <span class="text-category">Graphic Design</span>
-                                    </div>
-                                    <div class="caption-bg"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio identity" data-cat="identity" href="#portfolioModal1" data-toggle="modal">
-                            <div class="portfolio-wrapper">
-                                <img src="assets/img/portfolio/grid/identity/2.jpg" alt="">
-                                <div class="caption">
-                                    <div class="caption-text">
-                                        <a class="text-title">Demish</a>
-                                        <span class="text-category">Brand Identity</span>
-                                    </div>
-                                    <div class="caption-bg"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio web" data-cat="web" href="#portfolioModal2" data-toggle="modal">
-                            <div class="portfolio-wrapper">
-                                <img src="assets/img/portfolio/grid/web/4.jpg" alt="">
-                                <div class="caption">
-                                    <div class="caption-text">
-                                        <a class="text-title">OneRepublic</a>
-                                        <span class="text-category">Web Development</span>
-                                    </div>
-                                    <div class="caption-bg"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio graphic" data-cat="graphic" href="#portfolioModal3" data-toggle="modal">
-                            <div class="portfolio-wrapper">
-                                <img src="assets/img/portfolio/grid/graphic/5.jpg" alt="">
-                                <div class="caption">
-                                    <div class="caption-text">
-                                        <a class="text-title">Postable</a>
-                                        <span class="text-category">Graphic Design</span>
-                                    </div>
-                                    <div class="caption-bg"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio identity" data-cat="identity" href="#portfolioModal4" data-toggle="modal">
-                            <div class="portfolio-wrapper">
-                                <img src="assets/img/portfolio/grid/identity/3.jpg" alt="">
-                                <div class="caption">
-                                    <div class="caption-text">
-                                        <a class="text-title">Simpli Nota</a>
-                                        <span class="text-category">Brand Identity</span>
-                                    </div>
-                                    <div class="caption-bg"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div class="col-md-6"> 
+                    <iframe width="420" height="315" src="https://www.youtube.com/watch?v=kMbv3bKxqgs"></iframe>
+                </div>
+                <div class="col-md-6"> 
+                    <iframe width="420" height="315" src="https://www.youtube.com/watch?v=uK-ewJq16to&feature=youtu.be"></iframe>
                 </div>
             </div>
+            
+
         </div>
     </section>
     <section id="contact">
