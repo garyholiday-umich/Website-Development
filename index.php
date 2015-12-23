73083671474dcdae2f5032a923569157e05ad2d2
Gary Johnson <gjjohnson13@gmail.com>
2015-12-23 17:26:22 -0500

    Fixed video widths

--- index.php
+++ index.php
@@ -160,19 +160,19 @@
             <br>
             <div class="row">
                 <div class="col-md-6"> 
-                    <iframe width="420" height="315" src="https://www.youtube.com/embed/kMbv3bKxqgs" frameborder="0" allowfullscreen></iframe>
+                    <iframe width="50%" height="315" src="https://www.youtube.com/embed/kMbv3bKxqgs" frameborder="0" allowfullscreen></iframe>
                 </div>
                 <div class="col-md-6"> 
-                    <iframe width="420" height="315" src="https://www.youtube.com/embed/fOB_Mo5c5ik" frameborder="0" allowfullscreen></iframe>
+                    <iframe width="50%" height="315" src="https://www.youtube.com/embed/fOB_Mo5c5ik" frameborder="0" allowfullscreen></iframe>
                 </div>
             </div>
             <br>
             <div class="row">
                 <div class="col-md-6"> 
-                    <iframe width="420" height="315" src="https://www.youtube.com/embed/uK-ewJq16to" frameborder="0" allowfullscreen></iframe>
+                    <iframe width="50%" height="315" src="https://www.youtube.com/embed/uK-ewJq16to" frameborder="0" allowfullscreen></iframe>
                 </div>
                 <div class="col-md-6"> 
-                    <iframe width="420" height="315" src="https://www.youtube.com/embed/MhpBPwrz86g" frameborder="0" allowfullscreen></iframe>
+                    <iframe width="50%" height="315" src="https://www.youtube.com/embed/MhpBPwrz86g" frameborder="0" allowfullscreen></iframe>
                 </div>
             </div>
             
